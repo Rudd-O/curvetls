78d00b891d063f6f8e44786918f8833adaa13fc6
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-09-27 04:54:49 +0000

    Split benchmarks into encrypt and decrypt.

--- curvezmq_test.go
+++ curvezmq_test.go
@@ -1,30 +1,50 @@
 package curvetls
 
 import (
-	"bytes"
 	"testing"
 )
 
-var benchmarkParms = validParms(nil)
-
-func benchmarkMessageEncryptDecrypt(msgsize int, b *testing.B) {
+func benchmarkMessageEncrypt(msgsize int, b *testing.B) {
 	b.SetBytes(int64(msgsize))
-	p, f := benchmarkParms, &messageCommand{}
+	p, f := validParms(b), &messageCommand{}
 	in := make([]byte, msgsize)
 	inS := in[:]
-	var out []byte
+	var err error
 	for n := 0; n < b.N; n++ {
 		f.build(p.sN, p.sPriv, p.cPub, inS, true)
-		out, _ = f.validate(p.cN, p.cPriv, p.sPub, true)
 	}
-	if bytes.Compare(inS, out) != 0 {
-		b.Fatalf("%s != %s", inS, out)
+	if err != nil {
+		b.Fatal(err)
 	}
 }
 
-func BenchmarkMessageEncryptDecrypt1B(b *testing.B)   { benchmarkMessageEncryptDecrypt(1, b) }
-func BenchmarkMessageEncryptDecrypt64B(b *testing.B)  { benchmarkMessageEncryptDecrypt(64, b) }
-func BenchmarkMessageEncryptDecrypt1KB(b *testing.B)  { benchmarkMessageEncryptDecrypt(1024, b) }
-func BenchmarkMessageEncryptDecrypt64KB(b *testing.B) { benchmarkMessageEncryptDecrypt(1024*64, b) }
-func BenchmarkMessageEncryptDecrypt1MB(b *testing.B)  { benchmarkMessageEncryptDecrypt(1024*1024, b) }
-func BenchmarkMessageEncryptDecrypt64MB(b *testing.B) { benchmarkMessageEncryptDecrypt(64*1024*1024, b) }
+func benchmarkMessageDecrypt(msgsize int, b *testing.B) {
+	b.SetBytes(int64(msgsize))
+	p, f := validParms(b), &messageCommand{}
+	in := make([]byte, msgsize)
+	f.build(p.sN, p.sPriv, p.cPub, in[:], true)
+	var err error
+	for n := 0; n < b.N; n++ {
+		_, err = f.validate(p.cN, p.cPriv, p.sPub, true)
+		p.cN.counter -= 1
+	}
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+
+func BenchmarkMessageEncrypt1B(b *testing.B)   { benchmarkMessageEncrypt(1, b) }
+func BenchmarkMessageEncrypt64B(b *testing.B)  { benchmarkMessageEncrypt(64, b) }
+func BenchmarkMessageEncrypt1KB(b *testing.B)  { benchmarkMessageEncrypt(1024, b) }
+func BenchmarkMessageEncrypt64KB(b *testing.B) { benchmarkMessageEncrypt(1024*64, b) }
+func BenchmarkMessageEncrypt1MB(b *testing.B)  { benchmarkMessageEncrypt(1024*1024, b) }
+func BenchmarkMessageEncrypt64MB(b *testing.B) { benchmarkMessageEncrypt(64*1024*1024, b) }
+
+func BenchmarkMessageDecrypt1B(b *testing.B)   { benchmarkMessageDecrypt(1, b) }
+func BenchmarkMessageDecrypt64B(b *testing.B)  { benchmarkMessageDecrypt(64, b) }
+func BenchmarkMessageDecrypt1KB(b *testing.B)  { benchmarkMessageDecrypt(1024, b) }
+func BenchmarkMessageDecrypt64KB(b *testing.B) { benchmarkMessageDecrypt(1024*64, b) }
+func BenchmarkMessageDecrypt1MB(b *testing.B)  { benchmarkMessageDecrypt(1024*1024, b) }
+func BenchmarkMessageDecrypt64MB(b *testing.B) { benchmarkMessageDecrypt(64*1024*1024, b) }
+
