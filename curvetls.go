e34264c8e80b01b840e3e31fd093128976b57ce3
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-09-27 21:08:54 +0000

    Clarified documentation a bit more.

--- curvetls.go
+++ curvetls.go
@@ -1,38 +1,36 @@
 // Package curvetls is a robust and simple transport encryption library.
 //
-// Package curvetls provides a pluggable wrapper (client / server)
-// for network I/O, which allows you to upgrade your regular network sockets
-// to a protocol that supports robust framing and transport security,
+// This is a pluggable wrapper (client / server) for network I/O, which
+// allows you to upgrade your regular network sockets to a protocol that
+// supports robust framing, transport security and authentication,
 // so long as your net.Conn is of any reliable kind (e.g. a TCP- or
 // file-backed `net.Conn`).
 //
 // Usage Instructions
 //
-// The general implementor's instructions go as follows:
-//
-// Generate keypairs for clients and server, persisting them to disk if
+// (a) Generate keypairs for clients and server, persisting them to disk if
 // you want to, so you can later load them again.
 //
-// Distribute, however you see fit, the public keys of the server to the
+// (b) Distribute, however you see fit, the public keys of the server to the
 // clients, and the public keys of the clients to the server.
 //
-// Generate one long nonce per server keypair, and one long nonce per
+// (c) Generate one long nonce per server keypair, and one long nonce per
 // client keypair.  You can do this at runtime.  Never reuse the
 // same long nonce for two different keypairs.
 //
-// Make your server Listen() on a TCP socket, and Accept() incoming
+// (d) Make your server Listen() on a TCP socket, and Accept() incoming
 // connections to obtain one or more server net.Conn.
 //
-// Make your clients Connect() on a TCP socket to the Listen() address
+// (e) Make your clients Connect() on a TCP socket to the Listen() address
 // of the server.
 //
-// On your client, right after Connect(), wrap the net.Conn you received
+// (f) On your client, right after Connect(), wrap the net.Conn you received
 // by using WrapClient() on that client net.Conn, and giving it the client
 // keypair, its corresponding client long nonce, and the server public key.
 // WrapClient() will return an encrypted socket you can use to talk to
 // the server.
 //
-// On your server, right after Accept(), wrap the net.Conn you received
+// (g) On your server, right after Accept(), wrap the net.Conn you received
 // by using WrapServer() on that server net.Conn, and giving it
 // the server keypair together with its corresponding server long nonce.
 // Use the authorizer and the public key that WrapServer() returns to
