60a3bd8e976ccad6b849b21e55e97416d5115006
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2016-09-27 19:23:40 +0000

    Quick benchmarks for NaCL.

--- curvezmq_test.go
+++ curvezmq_test.go
@@ -1,6 +1,7 @@
 package curvetls
 
 import (
+	"golang.org/x/crypto/nacl/box"
 	"testing"
 )
 
@@ -33,6 +34,30 @@ func benchmarkMessageDecrypt(msgsize int, b *testing.B) {
 	}
 }
 
+func benchmarkNaclKeypairEnc(msgsize int, b *testing.B) {
+	b.SetBytes(int64(msgsize))
+	p, _ := validParms(b), &messageCommand{}
+	in := make([]byte, msgsize)
+	var nonce [24]byte
+	sPriv := [32]byte(p.sPriv)
+	cPub := [32]byte(p.cPub)
+	for n := 0; n < b.N; n++ {
+		box.Seal(nil, in, &nonce, &sPriv, &cPub)
+	}
+}
+
+func benchmarkNaclKeypairDec(msgsize int, b *testing.B) {
+	b.SetBytes(int64(msgsize))
+	p, _ := validParms(b), &messageCommand{}
+	in := make([]byte, msgsize+box.Overhead)
+	var nonce [24]byte
+	sPriv := [32]byte(p.sPriv)
+	cPub := [32]byte(p.cPub)
+	for n := 0; n < b.N; n++ {
+		box.Open(nil, in, &nonce, &sPriv, &cPub)
+	}
+}
+
 func BenchmarkMessageEncrypt1B(b *testing.B)   { benchmarkMessageEncrypt(1, b) }
 func BenchmarkMessageEncrypt64B(b *testing.B)  { benchmarkMessageEncrypt(64, b) }
 func BenchmarkMessageEncrypt1KB(b *testing.B)  { benchmarkMessageEncrypt(1024, b) }
@@ -46,3 +71,11 @@ func BenchmarkMessageDecrypt1KB(b *testing.B)  { benchmarkMessageDecrypt(1024, b
 func BenchmarkMessageDecrypt64KB(b *testing.B) { benchmarkMessageDecrypt(1024*64, b) }
 func BenchmarkMessageDecrypt1MB(b *testing.B)  { benchmarkMessageDecrypt(1024*1024, b) }
 func BenchmarkMessageDecrypt64MB(b *testing.B) { benchmarkMessageDecrypt(64*1024*1024, b) }
+
+func BenchmarkNaclKeypairEnc1B(b *testing.B)   { benchmarkNaclKeypairEnc(1, b) }
+func BenchmarkNaclKeypairEnc64KB(b *testing.B) { benchmarkNaclKeypairEnc(64*1024, b) }
+func BenchmarkNaclKeypairEnc64MB(b *testing.B) { benchmarkNaclKeypairEnc(64*1024*1024, b) }
+
+func BenchmarkNaclKeypairDec1B(b *testing.B)   { benchmarkNaclKeypairDec(1, b) }
+func BenchmarkNaclKeypairDec64KB(b *testing.B) { benchmarkNaclKeypairDec(64*1024, b) }
+func BenchmarkNaclKeypairDec64MB(b *testing.B) { benchmarkNaclKeypairDec(64*1024*1024, b) }
